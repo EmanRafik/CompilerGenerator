784c6fff8d40c00cfe30cb22165718b501662da7
AbanoubZaki <abanoub4055@gmail.com>
2020-03-23 19:46:35 +0200

    Minimization done except returninig the new DFA

---
+++ cmake-build-debug/CMakeFiles/compilergenerator.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/DELL/CLionProjects/compilergenerator/main.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/main.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/DFA.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/DFA.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Files_manager.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Files_manager.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/ID.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/ID.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Identifier.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Identifier.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Keyword.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Keyword.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Lexical_analyzer.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Lexical_analyzer.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/NFA.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/NFA.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Number.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Number.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Punctuation_symbol.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Punctuation_symbol.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Regular_definition.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Regular_definition.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Regular_expression.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Regular_expression.cpp.obj"
+  "C:/Users/DELL/CLionProjects/compilergenerator/src/Token.cpp" "C:/Users/DELL/CLionProjects/compilergenerator/cmake-build-debug/CMakeFiles/compilergenerator.dir/src/Token.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/compilergenerator.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compilergenerator.dir/main.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/DFA.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Files_manager.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/ID.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Identifier.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Keyword.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Lexical_analyzer.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/NFA.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Number.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Punctuation_symbol.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Regular_definition.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Regular_expression.cpp.obj"
+  "CMakeFiles/compilergenerator.dir/src/Token.cpp.obj"
+  "compilergenerator.exe"
+  "compilergenerator.exe.manifest"
+  "compilergenerator.pdb"
+  "libcompilergenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compilergenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
