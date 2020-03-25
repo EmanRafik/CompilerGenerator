c75ee8ac02facf1e2974ed82358fa82e3028c15c
AbanoubZaki <abanoub4055@gmail.com>
2020-03-25 02:04:40 +0200

    fixing cmake issue

--- main.cpp
+++ main.cpp
@@ -7,55 +7,13 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    /*Lexical_analyzer_generator *generator = new Lexical_analyzer_generator();
+    Lexical_analyzer_generator *generator = new Lexical_analyzer_generator();
     generator->read_lexical_rules("rules.txt");
     generator->generate_lexical_analyzer();
-<<<<<<< HEAD
-=======
-//    Lexical_analyzer_generator *generator = new Lexical_analyzer_generator();
-//    generator->read_lexical_rules("rules.txt");
-//    generator->generate_lexical_analyzer();
->>>>>>> f633e9cd911747703b2d80c2d418c4a2a7672020
-////    DFA *dfa = generator->get_minimal_dfa();
-//    Lexical_analyzer *lexical = new Lexical_analyzer();
-//
-//    DFA *dfa = new DFA(6, 95);
-//    dfa->addTransition(0,0,1);
-//    dfa->addTransition(1,0,0);
-//    dfa->addTransition(2,0,4);
-//    dfa->addTransition(3,0,4);
-//    dfa->addTransition(4,0,4);
-//    dfa->addTransition(5,0,5);
-//
-//    dfa->addTransition(0,1,5);
-//    dfa->addTransition(1,1,1);
-//    dfa->addTransition(2,1,5);
-//    dfa->addTransition(3,1,5);
-//    dfa->addTransition(4,1,5);
-//    dfa->addTransition(5,1,5);
-//
-//    Token *t1 = new Token();
-//    t1->setToken_class("id");
-//    Token *t2 = new Token();
-//    t2->setToken_class("num");
-//    Token *t3 = new Token();
-//    t3->setToken_class("relop");
-//    dfa->addAcceptState(2, *t1);
-//    dfa->addAcceptState(3, *t2);
-//    dfa->addAcceptState(4, *t3);
-//
-//    dfa->minimize().print_dfa();
-//
-//    lexical->setDFA(dfa);
-//    lexical->read_input("input.txt");
-<<<<<<< HEAD
-
-=======
 //    DFA *dfa = generator->get_minimal_dfa();
     Lexical_analyzer *lexical = new Lexical_analyzer();
 
-    DFA *dfa = new DFA(6, 95);
+    DFA *dfa = new DFA(6, 2);
     dfa->addTransition(0,0,1);
     dfa->addTransition(1,0,0);
     dfa->addTransition(2,0,4);
@@ -84,10 +42,6 @@ int main()
 
     lexical->setDFA(dfa);
     lexical->read_input("input.txt");
-*/
->>>>>>> 9a08d3edcfe9e313173cb7a0f9c8e2dc0434a61e
-=======
 
->>>>>>> f633e9cd911747703b2d80c2d418c4a2a7672020
     return 0;
 }
